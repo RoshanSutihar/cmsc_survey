db41b4ba7cff14a9f98835f56fc398002e56ad4a
Roshan Sutihar <roshansutihar@gmail.com>
2023-02-24 15:12:35 -0600

    base setup complete springboot left

--- test/app/src/main/java/com/example/test/CustomAdapter.java
+++ test/app/src/main/java/com/example/test/CustomAdapter.java
@@ -1,2 +1,61 @@
-package com.example.test;public class CustomAdapter {
+package com.example.test;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter<Question> {
+
+    private ArrayList<Question> questions;
+
+    public CustomAdapter(Context context, int resource, ArrayList<Question> questions) {
+        super(context, resource, questions);
+        this.questions = questions;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        Question question = questions.get(position);
+
+        TextView questionTextView = convertView.findViewById(R.id.question_text_view);
+        questionTextView.setText(question.getQuestion());
+
+        RadioGroup answersRadioGroup = convertView.findViewById(R.id.answers_radio_group);
+
+        RadioButton answer1RadioButton = convertView.findViewById(R.id.answer_1_radio_button);
+        answer1RadioButton.setText(question.getAnswer1());
+        answer1RadioButton.setTag('a');
+
+        RadioButton answer2RadioButton = convertView.findViewById(R.id.answer_2_radio_button);
+        answer2RadioButton.setText(question.getAnswer2());
+        answer2RadioButton.setTag('b');
+
+        RadioButton answer3RadioButton = convertView.findViewById(R.id.answer_3_radio_button);
+        answer3RadioButton.setText(question.getAnswer3());
+        answer3RadioButton.setTag('c');
+
+        RadioButton answer4RadioButton = convertView.findViewById(R.id.answer_4_radio_button);
+        answer4RadioButton.setText(question.getAnswer4());
+        answer4RadioButton.setTag('d');
+        return convertView;
+    }
+    @Override
+    public Question getItem(int position) {
+        return questions.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return questions.size();
+    }
 }
---
+++ test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    Button sendData;
+
+    TextView message;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // create sample data
+        ArrayList<Question> questions = new ArrayList<>();
+        questions.add(new Question("What is your name?", "Roshan", "Ram", "Shyam", "Hari"));
+        questions.add(new Question("What is your name?", "Ram", "Ram", "Shyam", "Hari"));
+
+        // create custom adapter and set to ListView
+        CustomAdapter adapter = new CustomAdapter(this, R.layout.list_item, questions);
+        listView = findViewById(R.id.list_view);
+        sendData = findViewById(R.id.sendData);
+        message = findViewById(R.id.message);
+        listView.setAdapter(adapter);
+
+
+
+        sendData.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                boolean allSelected = true;
+
+
+                                            // Check if all radio buttons are selected
+                                            for (int i = 0; i < listView.getAdapter().getCount(); i++) {
+                                                View itemView = listView.getChildAt(i);
+
+                                                RadioGroup answersRadioGroup = itemView.findViewById(R.id.answers_radio_group);
+
+                                                int checkedRadioButtonId = answersRadioGroup.getCheckedRadioButtonId();
+
+                                                if (checkedRadioButtonId == -1) {
+                                                    allSelected = false;
+                                                    break;
+                                                }
+                                            }
+
+                                            // If all radio buttons are selected, get the selected answer for each question
+                                            if (allSelected) {
+                                                for (int i = 0; i < listView.getAdapter().getCount(); i++) {
+                                                    View itemView = listView.getChildAt(i);
+
+                                                    RadioGroup answersRadioGroup = itemView.findViewById(R.id.answers_radio_group);
+
+                                                    int checkedRadioButtonId = answersRadioGroup.getCheckedRadioButtonId();
+
+                                                    if (checkedRadioButtonId != -1) {
+                                                        RadioButton checkedRadioButton = itemView.findViewById(checkedRadioButtonId);
+
+                                                        Object tag = checkedRadioButton.getTag();
+
+                                                        if (tag != null) {
+                                                            String selectedAnswer = tag.toString();
+                                                            message.setText("selected" + selectedAnswer + "\n");
+                                                            message.setTextColor(Color.parseColor("#006400"));
+                                                            // Do something with the selected answer
+                                                        }
+                                                    }
+                                                }
+                                            } else {
+                                                message.setText("Every questions must be answred!");
+                                                message.setTextColor(Color.parseColor("#FF0000"));
+                                                // Show error message that all questions need to be answered
+                                            }
+                                        }
+
+
+//                for (int i = 0; i < listView.getAdapter().getCount(); i++) {
+//
+//                    View itemView = listView.getChildAt(i);
+//
+//                    RadioGroup answersRadioGroup = itemView.findViewById(R.id.answers_radio_group);
+//
+//                    int checkedRadioButtonId = answersRadioGroup.getCheckedRadioButtonId();
+//
+//                    if (checkedRadioButtonId != -1) {
+//                        RadioButton checkedRadioButton = itemView.findViewById(checkedRadioButtonId);
+//
+//                        Object tag = checkedRadioButton.getTag();
+//
+//                        if (tag != null) {
+//                            String selectedAnswer = tag.toString();
+//                            message.setText("Nothing selected"+ selectedAnswer);
+//                            // Do something with the selected answer
+//                        } else{
+//                            message.setText("Select Every option");
+//
+//                        }
+//
+//                    }
+                               //     }
+//                }
+
+
+
+
+
+        });
+    }
+}
