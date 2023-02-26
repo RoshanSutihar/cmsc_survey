fa31a47fce5997536c48b25bcffec8adecf6326b
Roshan Sutihar <roshansutihar@gmail.com>
2023-02-25 22:11:06 -0600

    gson working

--- test/app/src/main/java/com/example/test/MainActivity.java
+++ test/app/src/main/java/com/example/test/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.test;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +9,16 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
     Button sendData;
-
+    private Gson gson;
     TextView message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         message = findViewById(R.id.message);
         listView.setAdapter(adapter);
 
+        gson = new Gson();
 
 
         sendData.setOnClickListener(new View.OnClickListener() {
@@ -43,76 +46,48 @@ public class MainActivity extends AppCompatActivity {
                 boolean allSelected = true;
 
 
-                                            // Check if all radio buttons are selected
-                                            for (int i = 0; i < listView.getAdapter().getCount(); i++) {
-                                                View itemView = listView.getChildAt(i);
+                // Check if all radio buttons are selected
+                for (int i = 0; i < listView.getAdapter().getCount(); i++) {
+                    View itemView = listView.getChildAt(i);
 
-                                                RadioGroup answersRadioGroup = itemView.findViewById(R.id.answers_radio_group);
+                    RadioGroup answersRadioGroup = itemView.findViewById(R.id.answers_radio_group);
 
-                                                int checkedRadioButtonId = answersRadioGroup.getCheckedRadioButtonId();
+                    int checkedRadioButtonId = answersRadioGroup.getCheckedRadioButtonId();
 
-                                                if (checkedRadioButtonId == -1) {
-                                                    allSelected = false;
-                                                    break;
-                                                }
-                                            }
+                    if (checkedRadioButtonId == -1) {
+                        allSelected = false;
+                        break;
+                    }
+                }
 
-                                            // If all radio buttons are selected, get the selected answer for each question
-                                            if (allSelected) {
-                                                for (int i = 0; i < listView.getAdapter().getCount(); i++) {
-                                                    View itemView = listView.getChildAt(i);
+                // If all radio buttons are selected, get the selected answer for each question
+                if (allSelected) {
+                    for (int i = 0; i < listView.getAdapter().getCount(); i++) {
+                        View itemView = listView.getChildAt(i);
 
-                                                    RadioGroup answersRadioGroup = itemView.findViewById(R.id.answers_radio_group);
+                        RadioGroup answersRadioGroup = itemView.findViewById(R.id.answers_radio_group);
 
-                                                    int checkedRadioButtonId = answersRadioGroup.getCheckedRadioButtonId();
+                        int checkedRadioButtonId = answersRadioGroup.getCheckedRadioButtonId();
 
-                                                    if (checkedRadioButtonId != -1) {
-                                                        RadioButton checkedRadioButton = itemView.findViewById(checkedRadioButtonId);
+                        if (checkedRadioButtonId != -1) {
+                            RadioButton checkedRadioButton = itemView.findViewById(checkedRadioButtonId);
 
-                                                        Object tag = checkedRadioButton.getTag();
+                            Object tag = checkedRadioButton.getTag();
 
-                                                        if (tag != null) {
-                                                            String selectedAnswer = tag.toString();
-                                                            message.setText("selected" + selectedAnswer + "\n");
-                                                            message.setTextColor(Color.parseColor("#006400"));
-                                                            // Do something with the selected answer
-                                                        }
-                                                    }
-                                                }
-                                            } else {
-                                                message.setText("Every questions must be answred!");
-                                                message.setTextColor(Color.parseColor("#FF0000"));
-                                                // Show error message that all questions need to be answered
-                                            }
-                                        }
-
-
-//                for (int i = 0; i < listView.getAdapter().getCount(); i++) {
-//
-//                    View itemView = listView.getChildAt(i);
-//
-//                    RadioGroup answersRadioGroup = itemView.findViewById(R.id.answers_radio_group);
-//
-//                    int checkedRadioButtonId = answersRadioGroup.getCheckedRadioButtonId();
-//
-//                    if (checkedRadioButtonId != -1) {
-//                        RadioButton checkedRadioButton = itemView.findViewById(checkedRadioButtonId);
-//
-//                        Object tag = checkedRadioButton.getTag();
-//
-//                        if (tag != null) {
-//                            String selectedAnswer = tag.toString();
-//                            message.setText("Nothing selected"+ selectedAnswer);
-//                            // Do something with the selected answer
-//                        } else{
-//                            message.setText("Select Every option");
-//
-//                        }
-//
-//                    }
-                               //     }
-//                }
-
+                            if (tag != null) {
+                                String selectedAnswer = tag.toString();
+                                message.setText("selected" + selectedAnswer + "\n");
+                                message.setTextColor(Color.parseColor("#006400"));
+                                // Do something with the selected answer
+                            }
+                        }
+                    }
+                } else {
+                    message.setText("Every questions must be answred!");
+                    message.setTextColor(Color.parseColor("#FF0000"));
+                    // Show error message that all questions need to be answered
+                }
+            }
 
 
 
